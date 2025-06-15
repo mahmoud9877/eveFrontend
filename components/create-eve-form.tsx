70a9cf1c91893a7beedaafe9151dbe02f995d499
mahmoud9877 <mohmoudali9877@gmail.com>
2025-06-15 20:08:41 +0300

    wwww

--- components/create-eve-form.tsx
+++ components/create-eve-form.tsx
@@ -135,7 +135,7 @@ const CreateEveForm = () => {
     } finally {
       setIsSubmitting(false);
     }
-  };s
+  };
 
   return (
     // <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
